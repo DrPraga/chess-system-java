0f2174f51b5c8d873e1beb40ae61749d9afbf5a8
DrPraga <drpragaofficial@gmail.com>
2021-04-16 12:14:49 -0300

    Chess layer and printing the board Re-UP

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,5 +1,21 @@
 package chess;
 
-public class ChessPiece {
+import boardgame.Board;
+import boardgame.Piece;
 
+public class ChessPiece extends Piece {
+	
+	private Color colo;
+
+	public ChessPiece(Board board, Color colo) {
+		super(board);
+		this.colo = colo;
+	}
+
+	public Color getColo() {
+		return colo;
+	}
+
+	
+	
 }
